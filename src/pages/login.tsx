c027dddcd0c0e4d6fd0e71ebb149807b8a95b90a
Daniel-Dief <danieldiefsantos@gmail.com>
2026-09-22 18:51:22 -0300

    refactor: criação de store para tema

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,25 +2,33 @@ import { Card } from "@heroui/react";
 import { LoginForm, RegisterForm } from "@/components/forms";
 import { useState } from "react";
 import GradientWaves from "@/components/ui/GradientWaves";
+import { ThemeStore } from "@/stores/themeStore";
 
 export default function LoginPage() {
   const [isLogin, setIsLogin] = useState(true);
+  const { theme } = ThemeStore();
+
+  const colors = {
+    horizonColor: theme === "light" ? "#3300ff" : "#5227FF",
+    waveColor: theme === "light" ? "#3401ff" : "#FF9FFC",
+    crestColor: theme === "light" ? "#350ed4" : "#FFFFFF",
+  }
 
   return (
-    <div className="w-full h-screen relative flex justify-center items-center">
-      <div className="w-full h-[600px] absolute">
+    <div className={`w-full h-screen relative flex justify-center items-center ${theme === "light" ? "bg-white" : "bg-gray-900"}`}>
+      <div className="w-full h-full absolute">
         <GradientWaves
-          horizonColor="#5227FF"
-          waveColor="#FF9FFC"
-          crestColor="#FFFFFF"
+          horizonColor={colors.horizonColor}
+          waveColor={colors.waveColor}
+          crestColor={colors.crestColor}
           speed={0.4}
           amplitude={2.5}
           waveScale={0.6}
           waveRatio={0.9}
           swell={35}
           turbulence={20}
-          tilt={1.11}
-          zoom={1}
+          tilt={0.2}
+          zoom={0.7}
           height={5.5}
           fogDepth={15}
           detail="medium"
